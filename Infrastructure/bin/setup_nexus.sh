e9915fe38cbfd25567053f00fadc5622ca710bea
Eric Junker <ejunker@redhat.com>
2018-08-13 14:49:24 -0400

    nexus and sonar setup

--- Infrastructure/bin/setup_nexus.sh
+++ Infrastructure/bin/setup_nexus.sh
@@ -29,3 +29,55 @@ echo "Setting up Nexus in project $GUID-nexus"
 # oc new-app -f ../templates/nexus.yaml --param .....
 
 # To be Implemented by Student
+
+oc new-app sonatype/nexus3:latest -n $GUID-nexus
+oc expose svc nexus3  -n $GUID-nexus
+#pause rollout so we can do some patching
+oc rollout pause dc nexus3  -n $GUID-nexus
+oc patch dc nexus3 --patch='{ "spec": { "strategy": { "type": "Recreate" }}}'  -n $GUID-nexus
+oc set resources dc nexus3 --limits=memory=2Gi,cpu=2 --requests=memory=1Gi,cpu=500m -n $GUID-nexus
+
+#set up storage
+echo "apiVersion: v1
+kind: PersistentVolumeClaim
+metadata:
+  name: nexus-pvc
+spec:
+  accessModes:
+  - ReadWriteOnce
+  resources:
+    requests:
+      storage: 4Gi" | oc create -f - -n $GUID-nexus
+
+oc set volume dc/nexus3 --add --overwrite --name=nexus3-volume-1 --mount-path=/nexus-data/ --type persistentVolumeClaim --claim-name=nexus-pvc -n $GUID-nexus
+
+#set up liveness and readiness probes
+oc set probe dc/nexus3 --liveness --failure-threshold 3 --initial-delay-seconds 60 -- echo ok -n $GUID-nexus
+oc set probe dc/nexus3 --readiness --failure-threshold 3 --initial-delay-seconds 60 --get-url=http://:8081/repository/maven-public/ -n $GUID-nexus
+
+#resume rollout
+oc rollout resume dc nexus3 -n $GUID-nexus
+
+#check if Nexus is ready
+while : ; do
+  echo "Checking if Nexus is Ready..."
+  oc get pod -n ${GUID}-nexus|grep '\-2\-'|grep -v deploy|grep "1/1"
+  [[ "$?" == "1" ]] || break
+  echo "...no. Sleeping 10 seconds."
+  sleep 10
+done
+echo "...Yes!"
+
+#configure Nexus
+curl -o setup_nexus3.sh -s https://raw.githubusercontent.com/wkulhanek/ocp_advanced_development_resources/master/nexus/setup_nexus3.sh
+chmod +x setup_nexus3.sh
+./setup_nexus3.sh admin admin123 http://$(oc get route nexus3 --template='{{ .spec.host }}')
+rm setup_nexus3.sh
+
+#set up the registry
+oc expose dc nexus3 --port=5000 --name=nexus-registry -n $GUID-nexus
+oc create route edge nexus-registry --service=nexus-registry --port=5000 -n $GUID-nexus
+
+#annotate the routes
+oc annotate route nexus3 console.alpha.openshift.io/overview-app-route=true -n $GUID-nexus
+oc annotate route nexus-registry console.alpha.openshift.io/overview-app-route=false -n $GUID-nexus
--- Infrastructure/bin/setup_sonar.sh
+++ Infrastructure/bin/setup_sonar.sh
@@ -14,3 +14,35 @@ echo "Setting up Sonarqube in project $GUID-sonarqube"
 # oc new-app -f ../templates/sonarqube.yaml --param .....
 
 # To be Implemented by Student
+#set up PostgreSQL DB to back up Sonarqube
+oc new-app --template=postgresql-persistent --param POSTGRESQL_USER=sonar --param POSTGRESQL_PASSWORD=sonar --param POSTGRESQL_DATABASE=sonar --param VOLUME_CAPACITY=4Gi --labels=app=sonarqube_db -n $GUID-sonar
+#now set up Sonarqube itself
+oc new-app --docker-image=wkulhanek/sonarqube:6.7.4 --env=SONARQUBE_JDBC_USERNAME=sonar --env=SONARQUBE_JDBC_PASSWORD=sonar --env=SONARQUBE_JDBC_URL=jdbc:postgresql://postgresql/sonar --labels=app=sonarqube -n $GUID-sonar
+
+#pause rollout for some patching
+oc rollout pause dc sonarqube -n $GUID-sonar
+oc expose service sonarqube -n $GUID-sonar
+
+#set up storage for Sonarqube
+echo "apiVersion: v1
+kind: PersistentVolumeClaim
+metadata:
+  name: sonarqube-pvc
+spec:
+  accessModes:
+  - ReadWriteOnce
+  resources:
+    requests:
+      storage: 4Gi" | oc create -f - -n $GUID-sonar
+oc set volume dc/sonarqube --add --overwrite --name=sonarqube-volume-1 --mount-path=/opt/sonarqube/data/ --type persistentVolumeClaim --claim-name=sonarqube-pvc -n $GUID-sonar
+
+#resources and strategy
+oc set resources dc/sonarqube --limits=memory=3Gi,cpu=2 --requests=memory=2Gi,cpu=1 -n $GUID-sonar
+oc patch dc sonarqube --patch='{ "spec": { "strategy": { "type": "Recreate" }}}' -n $GUID-sonar
+
+#probes
+oc set probe dc/sonarqube --liveness --failure-threshold 3 --initial-delay-seconds 40 -- echo ok -n $GUID-sonar
+oc set probe dc/sonarqube --readiness --failure-threshold 3 --initial-delay-seconds 20 --get-url=http://:9000/about -n $GUID-sonar
+
+#resume deployment
+oc rollout resume dc sonarqube  -n $GUID-sonar
