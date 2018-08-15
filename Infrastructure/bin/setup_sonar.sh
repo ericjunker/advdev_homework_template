850ce423d3ea82b4cc0616ada1f7733f9f3d3bd9
Eric Junker <ejunker@redhat.com>
2018-08-15 12:56:39 -0400

    I am a gigantic idiot. resolving permissions issues by making sure that my namespace specifiers in OC command matched the actual project name

--- Infrastructure/bin/setup_sonar.sh
+++ Infrastructure/bin/setup_sonar.sh
@@ -15,17 +15,17 @@ echo "Setting up Sonarqube in project $GUID-sonarqube"
 
 # To be Implemented by Student
 #set up PostgreSQL DB to back up Sonarqube
-# oc new-app --template=postgresql-persistent --param POSTGRESQL_USER=sonar --param POSTGRESQL_PASSWORD=sonar --param POSTGRESQL_DATABASE=sonar --param VOLUME_CAPACITY=4Gi --labels=app=sonarqube_db -n $GUID-sonar
-oc create -f Infrastructure/templates/sonar/postgresql.yaml -n $GUID-sonar
+# oc new-app --template=postgresql-persistent --param POSTGRESQL_USER=sonar --param POSTGRESQL_PASSWORD=sonar --param POSTGRESQL_DATABASE=sonar --param VOLUME_CAPACITY=4Gi --labels=app=sonarqube_db -n $GUID-sonarqube
+oc create -f Infrastructure/templates/sonar/postgresql.yaml -n $GUID-sonarqube
 
 #now set up Sonarqube itself
-# oc new-app wkulhanek/sonarqube:6.7.4 --env=SONARQUBE_JDBC_USERNAME=sonar --env=SONARQUBE_JDBC_PASSWORD=sonar --env=SONARQUBE_JDBC_URL=jdbc:postgresql://postgresql/sonar --labels=app=sonarqube -n $GUID-sonar
+# oc new-app wkulhanek/sonarqube:6.7.4 --env=SONARQUBE_JDBC_USERNAME=sonar --env=SONARQUBE_JDBC_PASSWORD=sonar --env=SONARQUBE_JDBC_URL=jdbc:postgresql://postgresql/sonar --labels=app=sonarqube -n $GUID-sonarqube
 
-oc create -f Infrastructure/templates/sonar/sonar.yaml -n $GUID-sonar
+oc create -f Infrastructure/templates/sonar/sonar.yaml -n $GUID-sonarqube
 
 #pause rollout for some patching
-oc rollout pause dc sonarqube -n $GUID-sonar
-oc expose service sonarqube -n $GUID-sonar
+oc rollout pause dc sonarqube -n $GUID-sonarqube
+oc expose service sonarqube -n $GUID-sonarqube
 
 #set up storage for Sonarqube
 echo "apiVersion: v1
@@ -37,16 +37,16 @@ spec:
   - ReadWriteOnce
   resources:
     requests:
-      storage: 4Gi" | oc create -f - -n $GUID-sonar
-oc set volume dc/sonarqube --add --overwrite --name=sonarqube-volume-1 --mount-path=/opt/sonarqube/data/ --type persistentVolumeClaim --claim-name=sonarqube-pvc -n $GUID-sonar
+      storage: 4Gi" | oc create -f - -n $GUID-sonarqube
+oc set volume dc/sonarqube --add --overwrite --name=sonarqube-volume-1 --mount-path=/opt/sonarqube/data/ --type persistentVolumeClaim --claim-name=sonarqube-pvc -n $GUID-sonarqube
 
 #resources and strategy
-oc set resources dc/sonarqube --limits=memory=3Gi,cpu=2 --requests=memory=2Gi,cpu=1 -n $GUID-sonar
-oc patch dc sonarqube --patch='{ "spec": { "strategy": { "type": "Recreate" }}}' -n $GUID-sonar
+oc set resources dc/sonarqube --limits=memory=3Gi,cpu=2 --requests=memory=2Gi,cpu=1 -n $GUID-sonarqube
+oc patch dc sonarqube --patch='{ "spec": { "strategy": { "type": "Recreate" }}}' -n $GUID-sonarqube
 
 #probes
-oc set probe dc/sonarqube --liveness --failure-threshold 3 --initial-delay-seconds 40 -- echo ok -n $GUID-sonar
-oc set probe dc/sonarqube --readiness --failure-threshold 3 --initial-delay-seconds 20 --get-url=http://:9000/about -n $GUID-sonar
+oc set probe dc/sonarqube --liveness --failure-threshold 3 --initial-delay-seconds 40 -- echo ok -n $GUID-sonarqube
+oc set probe dc/sonarqube --readiness --failure-threshold 3 --initial-delay-seconds 20 --get-url=http://:9000/about -n $GUID-sonarqube
 
 #resume deployment
-oc rollout resume dc sonarqube  -n $GUID-sonar
+oc rollout resume dc sonarqube  -n $GUID-sonarqube
