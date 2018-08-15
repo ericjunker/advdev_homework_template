09288f2fc21ea7a1727e03c961f9a362d78d1a49
Eric Junker <ejunker@redhat.com>
2018-08-15 12:38:25 -0400

    continuing with template strategy

--- Infrastructure/bin/setup_sonar.sh
+++ Infrastructure/bin/setup_sonar.sh
@@ -16,10 +16,12 @@ echo "Setting up Sonarqube in project $GUID-sonarqube"
 # To be Implemented by Student
 #set up PostgreSQL DB to back up Sonarqube
 # oc new-app --template=postgresql-persistent --param POSTGRESQL_USER=sonar --param POSTGRESQL_PASSWORD=sonar --param POSTGRESQL_DATABASE=sonar --param VOLUME_CAPACITY=4Gi --labels=app=sonarqube_db -n $GUID-sonar
-oc create -f Infrastructure/templates/sonar/db/project.yaml
+oc create -f Infrastructure/templates/sonar/postgresql.yaml -n $GUID-sonar
 
 #now set up Sonarqube itself
-oc new-app wkulhanek/sonarqube:6.7.4 --env=SONARQUBE_JDBC_USERNAME=sonar --env=SONARQUBE_JDBC_PASSWORD=sonar --env=SONARQUBE_JDBC_URL=jdbc:postgresql://postgresql/sonar --labels=app=sonarqube -n $GUID-sonar
+# oc new-app wkulhanek/sonarqube:6.7.4 --env=SONARQUBE_JDBC_USERNAME=sonar --env=SONARQUBE_JDBC_PASSWORD=sonar --env=SONARQUBE_JDBC_URL=jdbc:postgresql://postgresql/sonar --labels=app=sonarqube -n $GUID-sonar
+
+oc create -f Infrastructure/templates/sonar/sonar.yaml -n $GUID-sonar
 
 #pause rollout for some patching
 oc rollout pause dc sonarqube -n $GUID-sonar
