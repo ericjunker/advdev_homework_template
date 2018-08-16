0a4a4a76124095e3b711de3cad3ff93effdf38be
Eric Junker <ejunker@redhat.com>
2018-08-16 11:36:37 -0400

    specified namespace

--- Infrastructure/bin/setup_dev.sh
+++ Infrastructure/bin/setup_dev.sh
@@ -14,7 +14,7 @@ echo "Setting up Parks Development Environment in project ${GUID}-parks-dev"
 # To be Implemented by Student
 
 #create a MongoDB database
-oc create -f Infrastructure/templates/dev/mongo.yaml
-oc expose svc/mongodb
+oc create -f Infrastructure/templates/dev/mongo.yaml -n ${GUID}-parks-dev
+oc expose svc/mongodb -n ${GUID}-parks-dev
 #label the service so that apps can find it
-oc label service mongodb type=parksmap-backend
+oc label service mongodb type=parksmap-backend -n ${GUID}-parks-dev
