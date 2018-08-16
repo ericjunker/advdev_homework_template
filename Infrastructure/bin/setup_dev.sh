d7d2ec4379e943009532b87d18e63b129fcc7e6d
Eric Junker <ejunker@redhat.com>
2018-08-16 11:34:27 -0400

    mongodb for dev environment

--- Infrastructure/bin/setup_dev.sh
+++ Infrastructure/bin/setup_dev.sh
@@ -12,3 +12,9 @@ echo "Setting up Parks Development Environment in project ${GUID}-parks-dev"
 # Code to set up the parks development project.
 
 # To be Implemented by Student
+
+#create a MongoDB database
+oc create -f Infrastructure/templates/dev/mongo.yaml
+oc expose svc/mongodb
+#label the service so that apps can find it
+oc label service mongodb type=parksmap-backend
