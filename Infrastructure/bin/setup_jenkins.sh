54ae07213846d06cdad30e7fc19419f9702d931f
Eric Junker <ejunker@redhat.com>
2018-08-21 14:43:12 -0400

    still can't connect to docker registry

--- Infrastructure/bin/setup_jenkins.sh
+++ Infrastructure/bin/setup_jenkins.sh
@@ -31,7 +31,6 @@ echo "Setting up Jenkins in project ${GUID}-jenkins from Git Repo ${REPO} for Cl
 oc new-app jenkins-persistent --param ENABLE_OAUTH=true --param MEMORY_LIMIT=2Gi --param VOLUME_CAPACITY=4Gi -n $GUID-jenkins
 #jenkins is hungry. give it some more CPU
 oc patch dc jenkins -p '{"spec":{"template":{"spec":{"containers":[{"name":"jenkins","resources":{"limits":{"cpu":"2"}}}] }}}}'
-#Slave pod is located on docker hub, the Jenkinsfiles for each project need to refer to that image
 #create build configs
 oc create -f Infrastructure/templates/parksmap-pipeline.yaml -n $GUID-jenkins
 oc create -f Infrastructure/templates/nationalparks-pipeline.yaml -n $GUID-jenkins
